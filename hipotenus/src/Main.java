b4aeb0656aa9887faa38cc2fd46fa68213772d57
esmmrcn <114993200+esmmrcn@users.noreply.github.com>
2022-10-07 14:00:45 +0300

    üçgen çevre ve alan

--- hipotenus/src/Main.java
+++ hipotenus/src/Main.java
@@ -4,23 +4,26 @@ import java.lang.Math;
 public class Main {
     public static void main(String args[]){
 
-        double kenar1, kenar2, hipotenus, cevre, alan, u;
+        double kenar1, kenar2, kenar3, cevre, alan, u;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Üçgenin 1. dik kenarını giriniz: ");
+        System.out.print("Üçgenin 1. kenarını giriniz: ");
         kenar1 = input.nextDouble();
 
         Scanner input1 = new Scanner(System.in);
-        System.out.print("Üçgenin 2. dik kenarını giriniz: ");
+        System.out.print("Üçgenin 2. kenarını giriniz: ");
         kenar2 = input.nextDouble();
 
-        hipotenus= Math.sqrt(kenar1*kenar1 + kenar2*kenar2);
-        cevre=(kenar1+kenar2+hipotenus);
-        u=cevre/2;
-        alan= Math.sqrt(u*(u-kenar1)*(u-kenar2)*(u-hipotenus));
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Üçgenin 3. kenarını giriniz: ");
+        kenar3 = input.nextDouble();
 
-        System.out.println("Hipotenüs: "+hipotenus);
+        cevre=(kenar1+kenar2+kenar3);
+        u=cevre/2;
+        alan= Math.sqrt(u*(u-kenar1)*(u-kenar2)*(u-kenar3));
+
         System.out.println("Çevre: "+cevre);
         System.out.println("Alan: "+alan);
     }
 }
+
