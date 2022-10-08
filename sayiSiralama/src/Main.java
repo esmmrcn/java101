43b4ca4b2e7f08ea5f090cd1ebaf0254d9780fd9
esmmrcn <114993200+esmmrcn@users.noreply.github.com>
2022-10-08 23:11:44 +0300

    Update Main.java

--- sayiSiralama/src/Main.java
+++ sayiSiralama/src/Main.java
@@ -13,23 +13,23 @@ public class Main {
         System.out.print("Ucuncu sayiyi giriniz(c) :");
         c = keyboard.nextInt();
 
-        if (a>b && a>c){
-            if (b>c){
-                System.out.println("a>b>c");
+        if (a<b && a<c){
+            if (b<c){
+                System.out.println("a<b<c");
             }else {
-                System.out.println("a>c>b");
+                System.out.println("a<c<b");
             }
-        }else if (b>a && b>c){
-            if (a>c){
-                System.out.println("b>a>c");
+        }else if (b<a && b<c){
+            if (a<c){
+                System.out.println("b<a<c");
             }else {
-                System.out.println("b>c>a");
+                System.out.println("b<c<a");
             }
         }else {
-            if (a>b){
-                System.out.println("c>a>b");
+            if (a<b){
+                System.out.println("c<a<b");
             }else {
-                System.out.println("c>b>a");
+                System.out.println("c<b<a");
             }
         }
     }
