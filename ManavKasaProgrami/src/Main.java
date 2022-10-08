a2a7c66e22586a67c45c74b06ab79190a05a1ada
esmmrcn <114993200+esmmrcn@users.noreply.github.com>
2022-10-08 20:27:50 +0300

    Update Main.java

--- ManavKasaProgrami/src/Main.java
+++ ManavKasaProgrami/src/Main.java
@@ -5,24 +5,21 @@ public class Main {
 
         double armut=2.14, elma=3.67, domates=1.11, muz=0.95, patlıcan=5.0;
 
-        System.out.print("Armut kaç kilo ? :");
         Scanner input = new Scanner(System.in);
+        
+        System.out.print("Armut kaç kilo ? :");
         armut*= input.nextDouble();
 
         System.out.print("Elma kaç kilo ? :");
-        Scanner input1 = new Scanner(System.in);
         elma*= input.nextDouble();
 
         System.out.print("Domates kaç kilo ? :");
-        Scanner input2 = new Scanner(System.in);
         domates*= input.nextDouble();
 
         System.out.print("Muz kaç kilo ? :");
-        Scanner input3 = new Scanner(System.in);
         muz*= input.nextDouble();
 
         System.out.print("Patlıcan kaç kilo ? :");
-        Scanner input4 = new Scanner(System.in);
         patlıcan*= input.nextDouble();
 
         System.out.println("Toplam tutar : "+(armut+elma+domates+muz+patlıcan)+" TL");
