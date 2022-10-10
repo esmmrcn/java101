a7bb939848bc8a365d45e997232f5e2230de9451
esmmrcn <114993200+esmmrcn@users.noreply.github.com>
2022-10-10 19:49:38 +0300

    Add files via upload

---
+++ Donguler/src/Kombinasyon.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Kombinasyon {
+    public static void main(String[] args) {
+
+        int n, r, i;
+        int cr = 1;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kombinasyon almak için sayıları sırası ile giriniz (n r) :");
+        n = input.nextInt();
+        r = input.nextInt();
+
+        for (i = 1; i <= r; n--, r--) {
+            cr *= n;
+            cr /= r;
+        }
+
+        System.out.println("C(n,r)= "cr);
+    }
+}
